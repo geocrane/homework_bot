6b2193dc2bb38df69410aaba2709596e86cac972
Сергей Журавлев <geo-rnd@yandex.ru>
2022-10-01 22:37:22 +0300

    review 02

--- exceptions.py
+++ exceptions.py
@@ -1,34 +1,4 @@
-class StatusError(KeyError):
-    """Ошибка извлечения статуса проверки."""
-
-    message = "Ошибка извлечения статуса проверки"
-
-
-class NoDictError(TypeError):
-    """В ответе не получен словарь."""
-
-    message = "В ответе не получен словарь"
-
-
 class ServerError(Exception):
-    """Сервер API недоступен."""
+    """Сервер c API недоступен."""
 
-    message = "Сервер API недоступен"
-
-
-class ResponseError(Exception):
-    """Ответ API не соответсвует ожидаемому."""
-
-    message = "Ответ API не соответсвует ожидаемому"
-
-
-class EmptyList(Exception):
-    """Нет работ на проверке после указанной даты."""
-
-    message = "Работа не проверяется"
-
-
-class MessageError(Exception):
-    """Не удалось отправить сообщение."""
-
-    message = "Не удалось отправить сообщение"
+    message = "Сервер c API недоступен"
--- homework.py
+++ homework.py
@@ -1,25 +1,23 @@
+import inspect
 import logging
 import os
 import time
 import requests
 
 from dotenv import load_dotenv
+from logging.handlers import RotatingFileHandler
+from requests import (
+    ReadTimeout,
+    ConnectTimeout,
+    HTTPError,
+    Timeout,
+    ConnectionError,
+)
 from telegram import Bot
 
-from exceptions import (
-    EmptyList,
-    MessageError,
-    NoDictError,
-    ResponseError,
-    ServerError,
-    StatusError,
-)
+from exceptions import ServerError
 
 load_dotenv()
-
-logging.basicConfig(
-    level=logging.DEBUG, format="%(levelname)s, %(asctime)s, %(message)s"
-)
 
 PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
@@ -29,12 +27,27 @@ RETRY_TIME = 600
 ENDPOINT = "https://practicum.yandex.ru/api/user_api/homework_statuses/"
 HEADERS = {"Authorization": f"OAuth {PRACTICUM_TOKEN}"}
 
-
-HOMEWORK_STATUSES = {
+HOMEWORK_VERDICTES = {
     "approved": "Работа проверена: ревьюеру всё понравилось. Ура!",
     "reviewing": "Работа взята на проверку ревьюером.",
     "rejected": "Работа проверена: у ревьюера есть замечания.",
 }
+
+SEND_MESSAGE = "Отправлено сообщение: {message}"
+STATUS_CODE_ERR_MSG = (
+    "Ошибка! Код возврата: {code}.\nАдрес: {endpoint}, "
+    "headers: {headers}, timestamp: {timestamp}"
+)
+NOT_DICT_ERR_MSG = "Тип ответа не является словарем"
+NOT_LIST_ERR_MSG = "Неверный тип объекта. Ожидается: list. Получен: {type}"
+NO_ATTR_ERR_MSG = (
+    "Отсутствует необходимый атрибут. "
+    "Ожидается: 'homeworks'. Присутствуют: {attributes} "
+)
+NO_HOMEWORK_STATUS = (
+    "Неизвестный статус. Ожидается: {valid_statuses} Получен: {status}"
+)
+STATUS_CHANGE_MSG = 'Изменился статус проверки работы "{homework}". {status}'
 
 
 def send_message(bot, message):
@@ -42,11 +55,8 @@ def send_message(bot, message):
     Принимает на вход два параметра: экземпляр класса Bot и строку с текстом
     сообщения.
     """
-    try:
-        bot.send_message(TELEGRAM_CHAT_ID, message)
-        logging.info(f'Отправлено сообщение: "{message}"')
-    except MessageError:
-        logging.error(f'Не удалось отправить сообщение: "{message}"')
+    bot.send_message(TELEGRAM_CHAT_ID, message)
+    logging.info(SEND_MESSAGE.format(message=message))
 
 
 def get_api_answer(current_timestamp):
@@ -55,12 +65,29 @@ def get_api_answer(current_timestamp):
     запроса должна вернуть ответ API, преобразовав его из формата JSON к типам
     данных Python.
     """
-    timestamp = current_timestamp or int(time.time())
-    params = {"from_date": timestamp}
-    response = requests.get(ENDPOINT, headers=HEADERS, params=params)
+    params = {"from_date": current_timestamp}
+    try:
+        response = requests.get(ENDPOINT, headers=HEADERS, params=params)
+    except (
+        ConnectTimeout,
+        HTTPError,
+        ReadTimeout,
+        Timeout,
+        ConnectionError,
+    ) as err:
+        pass
+        logging.error(err)
     if not response.status_code == 200:
-        raise ServerError(logging.error("Сервер API недоступен"))
+        raise ServerError(
+            STATUS_CODE_ERR_MSG.format(
+                code=response.status_code,
+                endpoint=ENDPOINT,
+                headers=HEADERS,
+                timestamp=current_timestamp,
+            )
+        )
     response = response.json()
+    logging.info(response)
     return response
 
 
@@ -71,15 +98,20 @@ def check_response(response):
     вернуть список домашних работ (он может быть и пустым), доступный в ответе
     API по ключу 'homeworks'.
     """
-    if type(response) == list:
-        raise NoDictError
-    elif not type(response.get("homeworks")) == list:
-        logging.error("Домашки приходят не в виде списка")
-        raise ResponseError
-    elif "homeworks" not in response and "current_date" not in response:
-        logging.error("Отсутствуют ожидаемые ключи в ответе API")
-        raise ResponseError
-    return response.get("homeworks")
+    if not isinstance(response, dict):
+        raise TypeError(NOT_DICT_ERR_MSG)
+    if "homeworks" not in response:
+        raise AttributeError(
+            NO_ATTR_ERR_MSG.format(attributes=list(response.keys()))
+        )
+    homeworks = response.get("homeworks")
+    if not isinstance(homeworks, list):
+        raise TypeError(
+            NOT_LIST_ERR_MSG.format(
+                type=inspect.getmro(type(homeworks))[-2].__name__
+            )
+        )
+    return homeworks
 
 
 def parse_status(homework):
@@ -87,79 +119,74 @@ def parse_status(homework):
     В качестве параметра функция получает только один элемент из списка
     домашних работ. В случае успеха, функция возвращает подготовленную для
     отправки в Telegram строку, содержащую один из вердиктов словаря
-    HOMEWORK_STATUSES.
+    HOMEWORK_VERDICTES.
     """
-    if ("status" not in homework) and ("homework_name" not in homework):
-        logging.error("Отсутствует ожидаемый ключ")
-        raise StatusError
-    homework_status = homework.get("status")
-    homework_name = homework.get("homework_name")
-    if homework_status not in HOMEWORK_STATUSES:
-        logging.error("Недокументированный статус домашней работы")
-        raise StatusError
-    return (
-        f'Изменился статус проверки работы "'
-        f'{homework_name}". '
-        f"{HOMEWORK_STATUSES.get(homework_status)}"
+    status = homework["status"]
+    name = homework["homework_name"]
+    if status not in HOMEWORK_VERDICTES:
+        raise AttributeError(
+            NO_HOMEWORK_STATUS.format(
+                valid_statuses=list(HOMEWORK_VERDICTES.keys()), status=status
+            )
+        )
+    return STATUS_CHANGE_MSG.format(
+        homework=name, status=HOMEWORK_VERDICTES.get(status)
     )
 
 
 def check_tokens():
-    """Проверяет доступность необходимых переменных окружения.
-    Если отсутствует хотя бы одна переменная окружения — функция должна
-    вернуть False, иначе — True.
-    """
-    if not (PRACTICUM_TOKEN and TELEGRAM_TOKEN and TELEGRAM_CHAT_ID):
-        logging.critical(
-            "Отсутствует одна из обязательных переменных окружения"
-        )
+    """Проверяет доступность необходимых переменных окружения."""
+    if not TELEGRAM_CHAT_ID:
         return False
-    return True
-
-
-def message_sendler(bot, message, saved_message):
-    """Отправляет сообщение, если оно изменилось.
-    Если сообщение было отправлено ранее, повторной отправки не происходит,
-    иначе сообщение отправляется и возвращается для сохранения в переменную.
-    """
-    if not message == saved_message:
-        send_message(bot, message)
+    elif not TELEGRAM_TOKEN:
+        return False
+    elif not PRACTICUM_TOKEN:
+        return False
     else:
-        logging.debug("Статус не изменился")
-    time.sleep(RETRY_TIME)
-    return message
+        return True
 
 
 def main():
     """Основная логика работы бота."""
-    bot = Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = int(time.time())
-    saved_message = None
-    try:
-        while check_tokens():
+    if check_tokens():
+        bot = Bot(token=TELEGRAM_TOKEN)
+        current_timestamp = 0
+        saved_message = None
+        while True:
             try:
                 response = get_api_answer(current_timestamp)
                 homeworks = check_response(response)
-                if len(homeworks) == 0:
-                    raise EmptyList
-                message = parse_status(homeworks[0])
-                saved_message = message_sendler(bot, message, saved_message)
-                current_timestamp = response.get("current_date")
-            except (
-                ServerError,
-                StatusError,
-                ResponseError,
-                EmptyList,
-                MessageError,
-                NoDictError
-            ) as err:
-                saved_message = message_sendler(
-                    bot, err.message, saved_message
-                )
-                continue
-    except KeyboardInterrupt:
-        print(" Остановка бота")
+                if not len(homeworks) == 0:
+                    message = parse_status(homeworks[0])
+                    if message != saved_message:
+                        send_message(bot, message)
+                        saved_message = message
+                        current_timestamp = response.get(
+                            "current_date", current_timestamp
+                        )
+            except Exception as err:
+                logging.error(err)
+                if str(err) != saved_message:
+                    send_message(bot, str(err))
+                    saved_message = str(err)
+            time.sleep(RETRY_TIME)
+            continue
 
 
 if __name__ == "__main__":
+
+    logging.basicConfig(
+        level=logging.DEBUG,
+        handlers=[
+            RotatingFileHandler(
+                __file__ + ".log", maxBytes=5000000, backupCount=5
+            ),
+        ],
+        format=(
+            "[%(asctime)s][%(levelname)s][str: %(lineno)d]"
+            "[func: %(funcName)s] > %(message)s"
+        ),
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
     main()
